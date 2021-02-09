cccb5b7f2577478813a75b64762c85ab9e6e3bb6
Arghadeep <archakraborty@teksystems.com>
2021-02-09 13:57:41 +0530

    Base Commit.

---
+++ Parallax-UD/Parallax-UD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parallax-UD
+//
+//  Created by Arghadeep  on 09/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

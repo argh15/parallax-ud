083583c347ff573139f445921774a2ccae3e73c8
Arghadeep <archakraborty@teksystems.com>
2021-02-16 11:18:27 +0530

    Setup the parallax function.

--- Parallax-UD/Parallax-UD/View/ParallaxCell.swift
+++ Parallax-UD/Parallax-UD/View/ParallaxCell.swift
@@ -14,12 +14,33 @@ class ParallaxCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setupParallax()
     }
     
     func configureItemImage(withImage image: UIImage, andDescription description: String) {
         itemImage.image = image
         descriptionLabel.text = description
+    }
+    
+    func setupParallax() {
+        let min = CGFloat(-60)
+        let max = CGFloat(60)
+        
+        // x -> horizontal axis
+        let xMotion = UIInterpolatingMotionEffect(keyPath: "layer.transform.translation.x", type: .tiltAlongHorizontalAxis)
+        xMotion.maximumRelativeValue = max
+        xMotion.minimumRelativeValue = min
+        
+        // y -> horizontal axis
+        let yMotion = UIInterpolatingMotionEffect(keyPath: "layer.transform.translation.y", type: .tiltAlongVerticalAxis)
+        yMotion.maximumRelativeValue = max
+        yMotion.minimumRelativeValue = min
+        
+        let motionGroup = UIMotionEffectGroup()
+        motionGroup.motionEffects = [xMotion, yMotion]
+        
+        itemImage.addMotionEffect(motionGroup)
+        
     }
 
 }
